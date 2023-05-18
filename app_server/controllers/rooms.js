cac33b2515d007bab8a38a03a4cfa1c05e0f63fb
JSloan00 <jaelyn.sloan@snhu.edu>
2023-05-18 19:45:18 -0400

    Express website, static HTML only

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+
+var room = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travlr Getaways'});
+    res.render('rooms', {title: 'Travlr Getaways', room});
 };
 
 module.exports = {
