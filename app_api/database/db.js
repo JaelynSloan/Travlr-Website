557ec35f1257b794a9c421124ba983c3e1fbaeaf
JSloan00 <jaelyn.sloan@snhu.edu>
2023-05-29 13:59:48 -0400

    Create new application API

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose ');
+const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
 const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
